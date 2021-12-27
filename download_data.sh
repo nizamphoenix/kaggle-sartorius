6a17d3d51bec84b0123f3ced736c182f214a927e
Nizamuddin <nizamphoenix@gmail.com>
2021-12-27 23:43:40 +1000

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+pip install kaggle
 chmod 600 /home/ubuntu/.kaggle/kaggle.json
 cd kaggle-sartorius/data/raw
 kaggle competitions download -c sartorius-cell-instance-segmentation
@@ -9,3 +10,8 @@ mkdir cocopre
 cd ./cocopre/
 kaggle datasets download -d mikeskim/cocopre
 unzip cocopre.zip
+
+mkdir sartorius-train-tif
+cd ./sartorius-train-tif/
+kaggle datasets download -d ks2019/sartorius-train-tif
+unzip sartorius-train-tif
