3efd7a1027ba38f67d7f0bad8d7e1dd28e884fb0
nizamphoenix <nizamphoenix@gmail.com>
2021-12-28 10:19:20 +0000

    models for all 5 folds

--- download_data.sh
+++ download_data.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+git config --global user.email "nizamphoenix@gmail.com"
+git config --global user.name "nizamphoenix"
 pip install kaggle
+mkdir -p /home/ubuntu/.kaggle
+## do vim  /home/ubuntu/.kaggle/kaggle.json and copy kaggle api creds that's downloaded from kaggle 
 chmod 600 /home/ubuntu/.kaggle/kaggle.json
-cd kaggle-sartorius/data/raw
+cd /home/ubuntu/kaggle-sartorius/data/raw/
 kaggle competitions download -c sartorius-cell-instance-segmentation
 unzip ./sartorius-cell-instance-segmentation.zip
 
-mkdir cocopre
-cd ./cocopre/
+mkdir -p /home/ubuntu/kaggle-sartorius/data/raw/cocopre
+cd /home/ubuntu/kaggle-sartorius/data/raw/cocopre
 kaggle datasets download -d mikeskim/cocopre
 unzip cocopre.zip
 
-mkdir sartorius-train-tif
-cd ./sartorius-train-tif/
+mkdir -p /home/ubuntu/kaggle-sartorius/data/raw/sartorius-train-tif
+cd /home/ubuntu/kaggle-sartorius/data/raw/sartorius-train-tif/
 kaggle datasets download -d ks2019/sartorius-train-tif
 unzip sartorius-train-tif
 
+cd /home/ubuntu/kaggle-sartorius
